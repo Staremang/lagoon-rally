f587d575dc7920dab3a573ce4bc8d2d2e8329210
Мещеряков Роман <staremang@ya.ru>
2018-12-23 05:57:30 +0700

    FAQ

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+var widthPoint = {
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1230
+};
+
 function initHeader() {
     var $menu = $('.header-menu'),
         $menuOverlay = $('.header-menu-overlay'),
@@ -22,7 +29,7 @@ function initHeader() {
     function fixedHeader() {
         scrolled = window.pageYOffset || document.documentElement.scrollTop;
 
-        if (document.documentElement.clientWidth > 992) {
+        if (document.documentElement.clientWidth >= widthPoint.lg) {
             closeMenu();
             if (scrolled > 150) {
                 $header.addClass('fixed');
@@ -52,9 +59,9 @@ function initHeader() {
 };
 
 function init3dTour() {
-    var $panoTourButton     = $('.pano-tour__btn'),
-        $panoTourContainer  = $('.pano-tour__container'),
-        $panoTour           = $('.pano-tour'),
+    var $panoTourButton = $('.pano-tour__btn'),
+        $panoTourContainer = $('.pano-tour__container'),
+        $panoTour = $('.pano-tour'),
         panoTourOpened = false;
 
     $panoTourButton.on('click', function () {
@@ -68,21 +75,72 @@ function init3dTour() {
     })
 }
 
+function initBookingSlider() {
+    var $shipList = $('.booking-ships'),
+        sliderInitialized = false;
+
+
+    function buildSlider() {
+        if (document.documentElement.clientWidth < widthPoint.md) {
+
+            if (!sliderInitialized) {
+                $shipList.slick({
+                    centerMode: true,
+                    variableWidth: true,
+                    centerPadding: '60px',
+                    dots: true,
+                });
+                sliderInitialized = true;
+            }
+
+        } else {
+
+            if (sliderInitialized) {
+                $shipList.slick('unslick');
+                sliderInitialized = false;
+            }
+
+        }
+    }
+
+    buildSlider();
+    $(window).resize(buildSlider);
+}
+
+
 initHeader();
 init3dTour();
-
-$('.booking-ships').slick({
-
-    centerMode: true,
-    variableWidth: true,
-    centerPadding: '60px',
-    dots: true,
-});
+initBookingSlider();
 
 $('.gallery').slick({
 
-    centerMode: true,
-    variableWidth: true,
-    centerPadding: '60px',
-    dots: true,
+    slidesToScroll: 1,
+    slidesToShow: 4,
+    // variableWidth: true,
+    // centerPadding: '600px',
+
+    arrows: true,
+    centerMode: false,
+    dots: false,
+    infinite: false,
+    // draggable: false,
+    responsive: [
+        {
+            breakpoint: widthPoint.lg,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                dots: true,
+                // draggable: true
+                // centerPadding: '40px',
+                // slidesToShow: 1
+            }
+        }
+    ]
+});
+
+$('[data-fancybox="images"]').fancybox({
+    selector : '.slick-slide:not(.slick-cloned)',
+    animationEffect: "fade",
+    transitionEffect: "slide",
 });
